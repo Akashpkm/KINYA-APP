a16ac9d30aad7a563bc616a05cc44c4263d4c855
AKASH <ganeshsudha1224@gmail.com>
2025-10-06 17:33:08 +0530

    domain commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/KINYA-APP/'
+  base:'/kinyamedicalsystem.kinya.in'
 })
